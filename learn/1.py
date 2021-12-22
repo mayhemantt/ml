c5e8348aa1b90e72165c46673cd3418500a70cee
bornmay <mailhemantjoshi@gmail.com>
2021-12-22 13:37:35 +0530

    initial

---
+++ learn/1.py
@@ -0,0 +1,28 @@
+# print("hello world")
+
+x=6
+y="Hemant"
+# print(x, y)
+
+x=str(3)
+y=int(3)
+# print(x,y)
+
+x=int(6)
+y=int(9)
+# print(x + y)
+# print(float(x+y))
+
+# print(type(float(x+y)))
+
+x ="Hemant"
+y='Hemant'
+
+# print(x ==y)
+
+a ="4"
+A= "hemant"
+print(A+ a);
+"""
+Comment
+"""
